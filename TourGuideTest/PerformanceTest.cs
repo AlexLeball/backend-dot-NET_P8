4b1b9237e154c8f26959574ee733e43321043975
Alexandra Perrin <alexandrarose1@live.com.au>
2026-03-13 13:44:15 +0100

    update test

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -48,7 +48,7 @@ namespace TourGuideTest
         public async Task HighVolumeTrackLocation()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
-            _fixture.Initialize(1);
+            _fixture.Initialize(100000);
 
             List<User> allUsers = _fixture.TourGuideService.GetAllUsers();
 
@@ -75,7 +75,7 @@ namespace TourGuideTest
         public async Task HighVolumeGetRewards()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
-            _fixture.Initialize(1);
+            _fixture.Initialize(100000);
 
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
